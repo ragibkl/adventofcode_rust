d230f55b32e859b486beab1287254149e2c63f24
Ragib Badaruddin <ragib@seekasia.com>
2019-08-31 14:14:33 +0800

    solution day 3 p1

--- y2015_d3/src/main.rs
+++ y2015_d3/src/main.rs
@@ -1,3 +1,58 @@
+use std::collections::HashSet;
+use std::fs;
+
+#[derive(PartialEq, Eq, Hash, Debug)]
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+#[derive(Debug)]
+struct HouseGrid {
+    current: Point,
+    hash: HashSet<Point>,
+}
+
+impl HouseGrid {
+    fn new() -> HouseGrid {
+        let mut hash = HashSet::new();
+        hash.insert(Point { x: 0, y: 0 });
+
+        HouseGrid {
+            current: Point { x: 0, y: 0 },
+            hash,
+        }
+    }
+
+    fn walk_char(&mut self, c: char) {
+        match c {
+            '^' => self.current.y += 1,
+            'v' => self.current.y -= 1,
+            '>' => self.current.x += 1,
+            '<' => self.current.x -= 1,
+            _ => {}
+        };
+        self.hash.insert(Point {
+            x: self.current.x,
+            y: self.current.y,
+        });
+    }
+
+    fn count(&self) -> usize {
+        self.hash.len()
+    }
+}
+
+fn read_input_file() -> String {
+    let filename = "input.txt";
+    fs::read_to_string(filename).expect("Something went wrong reading the file")
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut grid = HouseGrid::new();
+    for c in read_input_file().chars() {
+        grid.walk_char(c);
+    }
+
+    println!("count = {}", grid.count());
 }
